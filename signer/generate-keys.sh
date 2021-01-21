936d331860f3edbeca513ea765c3cd6cdee44867
Rene Dekker <rene@tigera.io>
2021-01-21 11:38:31 -0800

    Add a simple signer for testing.

---
+++ signer/generate-keys.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+: ${1?'missing key directory'}
+
+key_dir="$1"
+
+chmod 0700 "$key_dir"
+cd "$key_dir"
+
+# Generate the CA cert and private key
+openssl req -nodes -new -x509 -keyout ca.key -out ca.crt -subj "/CN=cert-management-signer"
